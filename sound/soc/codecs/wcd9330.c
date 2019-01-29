9003e14043ae225deb554f6468aae690584d35aa
anupritaisno1 <www.anuprita804@gmail.com>
2019-01-29 11:54:16 +0000

    Revert "sound: codecs: PDesireAudio: Optimize code & Fix for init on Display state off" The state notifier driver this commit requires does not work
    This reverts commit afcc026f2af5444640dee218246bb61ca0aef006.
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- sound/soc/codecs/wcd9330.c
+++ sound/soc/codecs/wcd9330.c
@@ -47,14 +47,12 @@
 #include <linux/kernel.h>
 #include <linux/gpio.h>
 #include <sound/sounddebug.h>
-#include <linux/lcd_notify.h>
 #include "wcd9330.h"
 #include "wcd9xxx-resmgr.h"
 #include "wcd9xxx-common.h"
 #include "wcdcal-hwdep.h"
 #include "wcd_cpe_core.h"
 #include "pdesireaudio.h"
-
 
 enum {
 	VI_SENSE_1,
@@ -105,29 +103,6 @@ MODULE_PARM_DESC(cpe_debug_mode, "boot cpe in debug mode");
 
 static atomic_t kp_tomtom_priv;
 
-static struct notifier_block lcd_notifier_hook;
-static bool display_online;
-
-static int lcd_notifier_call(struct notifier_block *this,
-		unsigned long event, void *data)
-{
-	switch (event) {
-		case LCD_EVENT_ON_START:
-			display_online = true;
-			break;
-		case LCD_EVENT_OFF_END:
-			display_online = false;
-			break;
-		default:
-			break;
-
-	}
-
-	return 0;
-
-}
-
-
 int high_perf_mode = 1;
 module_param(high_perf_mode, int,
               S_IRUGO | S_IWUSR | S_IWGRP);
@@ -161,27 +136,27 @@ MODULE_PARM_DESC(pdesireaudio_static_mode, "Set PDesireAudio to static mode, so 
 
 void pdesireaudio_start(void) 
 {
-	if (!pdesireaudio_static_mode && display_online){
-		pr_info("Enable PDesireAudio");
+	if (!pdesireaudio_static_mode){
+		printk("Enable PDesireAudio");
 		uhqa_mode_pdesireaudio = 1;
 	}
 }
 
 void pdesireaudio_remove(void) 
 {
-	if (!pdesireaudio_static_mode && display_online){
-		pr_info("Disable PDesireAudio");
+	if (!pdesireaudio_static_mode){
+		printk("Disable PDesireAudio");
 		uhqa_mode_pdesireaudio = 0;
 	}
 } 
 
 void pdesireaudio_init(void) 
 {
-	if (!pdesireaudio_static_mode && display_online){
+	if (!pdesireaudio_static_mode){
 		bool active;
 
 
-		pr_info("Re-Init PDesireAudio");
+		printk("Re-Init PDesireAudio");
 		if (!uhqa_mode_pdesireaudio)
 			active = false;
 		else
@@ -199,10 +174,10 @@ void pdesireaudio_init(void)
 void pdesireaudio_api_static_mode_control(bool enable)
 {
 	if(enable == true) {
-		pr_info("Set PDesireAudio to static mode");
+		printk("Set PDesireAudio to static mode");
 		pdesireaudio_static_mode = 1;
 	} else {
-		pr_info("Set PDesireAudio to dynamic mode");
+		printk("Set PDesireAudio to dynamic mode");
 		pdesireaudio_static_mode = 0;
 	}
 }
@@ -1341,11 +1316,7 @@ static int tomtom_config_compander(struct snd_soc_dapm_widget *w,
 		}
 		
 		/* PDesireAudio Compander Switch */
-		if (uhqa_mode_pdesireaudio) {
-			pr_debug("%s: PDesireAudio is enabled, do not enable compander\n",
-					__func__);
-			break; 
-		}
+		if (!uhqa_mode_pdesireaudio) {
 		
 			/* Set compander Sample rate */
 			snd_soc_update_bits(codec,
@@ -1361,7 +1332,7 @@ static int tomtom_config_compander(struct snd_soc_dapm_widget *w,
 					snd_soc_update_bits(codec,
 						TOMTOM_A_CDC_COMP0_B4_CTL + (comp * 8),
 						0x80, 0x80);
-			
+				}
 			}
 			
 			/* Enable RX interpolation path compander clocks */
@@ -9486,9 +9457,6 @@ static struct platform_driver tomtom_codec_driver = {
 
 static int __init tomtom_codec_init(void)
 {
-	lcd_notifier_hook.notifier_call = lcd_notifier_call;
-	lcd_register_client(&lcd_notifier_hook);
-
 	return platform_driver_register(&tomtom_codec_driver);
 }
 
